b1d280b6e0feb5387e0826b58f9eb714c7dcb686
Jed Peek <jedpeek@yahoo.com>
2018-08-17 21:50:23 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -19,4 +19,14 @@ class SongsController < ApplicationController
       render :new
     end
   end
+
+  def update
+    @song = Song.find(params[:id])
+
+    if @song.save
+      redirect_to song_path(@song)
+    else
+      render :new
+    end
+  end
 end
