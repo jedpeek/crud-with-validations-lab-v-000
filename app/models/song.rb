e74a4c24f81081ac83d142de390ead95e499a942
Jed Peek <jedpeek@yahoo.com>
2018-08-17 21:54:14 +0000

    automatically backed up by learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,10 +1,10 @@
 class Song < ActiveRecord::Base
   validates :title, :artist_name presence: true
+  validates :released, inclusion: { in: [true, false] }
   validates :title, uniqueness: {
     scope: [:release_year, :artist_name],
     message: "cannot be repeated by the same artist in the same year"
   }
-  validates :released, inclusion: { in: [true, false] }
 
   with_options if: :released? do |song|
     song.validates :release_year, presence: true
